0c44379d47ab5091da5daab6099f887c8a50a96c
tatsuya <yokokura.tatsuya@gmail.com>
2020-01-18 07:10:19 +0900

    feat(list): MainAreaコンポーネントのstateを作成
    MainAreaコンポーネントの現在の状態を参照できるようにするためにstateを作成

--- src/js/components/mainArea.js
+++ src/js/components/mainArea.js
@@ -3,12 +3,20 @@ import Header from './header';
 import Footer from './footer';
 
 export default class MainArea extends React.Component {
-  render() {
-    const todos = [
-      { label: 'Todo1' },
-      { label: 'Todo2' },
-    ];
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      todos: [
+        { label: 'Todo1' },
+        { label: 'Todo2' },
+        { label: 'Todo3' },
+        { label: 'Todo4' }
+      ]
+    }
+  }
+
+  render() {
     let todoItemDom = [];
     for (let i = 0; i < todos.length; i++) {
       let todoItem = <li className="todo-list-item" key={"item-" + i}>{todos[i].label}</li>;
