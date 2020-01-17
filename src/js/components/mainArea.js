468d11fecba132827c1ab68650959e52ca833a17
tatsuya <yokokura.tatsuya@gmail.com>
2020-01-18 06:57:14 +0900

    feat(list): リストをループで表示

--- src/js/components/mainArea.js
+++ src/js/components/mainArea.js
@@ -4,13 +4,23 @@ import Footer from './footer';
 
 export default class MainArea extends React.Component {
   render() {
+    const todos = [
+      { label: 'Todo1' },
+      { label: 'Todo2' },
+    ];
+
+    let todoItemDom = [];
+    for (let i = 0; i < todos.length; i++) {
+      let todoItem = <li className="todo-list-item">{todos[i].label}</li>;
+      todoItemDom.push(todoItem);
+    }
+
     return (
       <div className="main-area">
         <Header />
         <main className="list-area">
           <ul className="todo-list">
-            <li className="todo-list-item">Todo1</li>
-            <li className="todo-list-item">Todo2</li>
+            {todoItemDom}
           </ul>
         </main>
         <Footer />
