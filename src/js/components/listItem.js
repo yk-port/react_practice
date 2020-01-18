483b432d142e4ad1bd9236e3d931432e8eeaed8b
tatsuya <yokokura.tatsuya@gmail.com>
2020-01-18 10:26:18 +0900

    feat(listItemComp): checkboxのイベントハンドラを作成

--- src/js/components/listItem.js
+++ src/js/components/listItem.js
@@ -1,10 +1,16 @@
 import React from 'react';
 
 export default class ListItem extends React.Component {
+
+  onChangeCheckBox(event) {
+    console.log('onChangeCheckBox');
+  }
+
   render() {
     return(
       <li className="todo-list-item">
-        <input type="checkbox"/>
+        <input type="checkbox"
+               onChange={this.onChangeCheckBox.bind(this)} />
         {this.props.data.label}
       </li>
     )
