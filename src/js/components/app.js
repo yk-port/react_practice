05392e02188dfb9747b2dca26f49bfe71c9bef54
tatsuya <yokokura.tatsuya@gmail.com>
2020-01-25 20:41:12 +0900

    feat(App.js): Appコンポーネント内に機能の関数を追加
    todoの追加、コンプリートのフラグ、削除機能

--- src/js/components/app.js
+++ src/js/components/app.js
@@ -25,12 +25,45 @@ export default class App extends React.Component {
           { id: 5, label: 'Todo5', completed: false },
         ]
       },
+      todoCount: 5,
       selectedGroup: 'group-1'
     }
   }
 
+  onAddTodo(label) {
+    let _state = Object.assign({}, this.state);
+    _state.todoCount++;
+    let todoList = _state.todoList[_state.selectedGroup];
+    let todoItem = { id: _state.todoCount, label, completed: false }
+    todoList.push(todoItem);
+    this.setState(_state);
+  }
+
+  onCompleteTodo(id) {
+    let _state = Object.assign({}, this.state);
+    let todoList = _state.todoList[_state.selectedGroup];
+    for (let i = 0; i < todoList.length; i++) {
+      if (todoList[i].id == id) {
+        todoList[i].completed = true;
+        break;
+      }
+    }
+    this.setState(_state);
+  }
+
+  onDeleteTodo(id) {
+    let _state = Object.assign({}, this.state);
+    let todoList = _state.todoList[_state.selectedGroup];
+    for (let i = 0; i < todoList.length; i++) {
+      if (todoList[i].id == id) {
+        todoList[i].splice(i, 1);
+        break;
+      }
+    }
+    this.setState(_state);
+  }
+
   onSelectGroup(id) {
-    console.log('onSelectGroup', id);
     this.setState({ selectedGroup: id });
   }
 
@@ -41,7 +74,10 @@ export default class App extends React.Component {
           groupList={this.state.groupList}
           onSelect={this.onSelectGroup.bind(this)} />
         <MainArea
-          todoList={this.state.todoList[this.state.selectedGroup]} />
+          todoList={this.state.todoList[this.state.selectedGroup]}
+          onAddTodo={this.onAddTodo.bind(this)}
+          onCompleteTodo={this.onCompleteTodo.bind(this)}
+          onDeleteTodo={this.onDeleteTodo.bind(this)} />
       </div>
     )
   }
