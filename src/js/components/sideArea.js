f176f7e0a4e17ded5beca919370d9a3dda2e1c9a
tatsuya <yokokura.tatsuya@gmail.com>
2020-01-25 18:39:03 +0900

    feat(sideArea): グループリストを作成

--- src/js/components/sideArea.js
+++ src/js/components/sideArea.js
@@ -1,9 +1,34 @@
 import React from 'react';
 
 export default class SideArea extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      groupList: [
+        { id: 'inbox', label: '受信箱' },
+        { id: 'group-1', label: 'グループ1' },
+      ]
+    }
+  }
+
+  renderGroup() {
+    let groupListDom = [];
+    for (let i = 0; i < this.state.groupList.length; i++) {
+      let group = this.state.groupList[i];
+      let groupList = <li key={group.id}>{group.label}</li>
+      groupListDom.push(groupList);
+    }
+    return groupListDom;
+  }
+
   render() {
     return (
-      <div className="side-area"></div>
+      <div className="side-area">
+        <ul className="group-list">
+          {this.renderGroup()}
+        </ul>
+      </div>
     )
   }
 }
