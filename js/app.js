a2286b894c14c6958c2134f64893c009dbd127d4
tatsuya <yokokura.tatsuya@gmail.com>
2020-03-07 12:07:39 +0900

    finish: 4-8 基本レイアウトの生成

--- js/app.js
+++ js/app.js
@@ -1,5 +1,10 @@
 ReactDOM.render(
-  <div>
+  <div className="wrap">
+    <div className="side-area"></div>
+    <div className="main-area">
+      <header className="header"></header>
+      <footer className="footer"></footer>
+    </div>
   </div>,
   document.getElementById('root')
 );
