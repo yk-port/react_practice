97a7ac42e570b835395ab2c961a20f1000434a97
tatsuya <yokokura.tatsuya@gmail.com>
2020-03-07 21:35:31 +0900

    6-23-1 グループのstateを用意してループ処理で表示

--- src/js/components/sideArea.js
+++ src/js/components/sideArea.js
@@ -1,9 +1,43 @@
 import React from 'react';
 
 export default class SideArea extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      groupList: [
+        {
+          id: 'inbox',
+          label: '受信箱',
+        },
+        {
+          id: 'group-1',
+          label: 'グループ1',
+        },
+        {
+          id: 'group-2',
+          label: 'グループ2',
+        },
+      ],
+    }
+  }
+
+  renderGroup() {
+    let groupListDom = [];
+    this.state.groupList.forEach(group => {
+      let groupItem = <li key={group.id}>{group.label}</li>;
+      groupListDom.push(groupItem);
+    });
+    return groupListDom;
+  }
+
   render() {
     return (
-      <div className="side-area"></div>
+      <div className="side-area">
+        <ul>
+          {this.renderGroup()}
+        </ul>
+      </div>
     );
   }
 }
