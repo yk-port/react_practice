c4e98534c6ebbf2e9ab7c9ed444e4ee5da398dd0
tatsuya <yokokura.tatsuya@gmail.com>
2020-03-07 12:12:08 +0900

    finish: 4-9 コンポーネントの作成

--- js/app.js
+++ js/app.js
@@ -1,10 +1,50 @@
+class Footer extends React.Component {
+  render() {
+    return (
+      <footer className="footer">&copy;2020 ReactPractice</footer>
+    );
+  }
+}
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header className="header">受信箱</header>
+    );
+  }
+}
+
+class SideArea extends React.Component {
+  render() {
+    return (
+      <div className="side-area"></div>
+    );
+  }
+}
+
+class MainArea extends React.Component {
+  render() {
+    return (
+      <div className="main-area">
+        <Header />
+        <Footer />
+      </div>
+    );
+  }
+}
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="wrap">
+        <SideArea />
+        <MainArea />
+      </div>
+    );
+  }
+}
+
 ReactDOM.render(
-  <div className="wrap">
-    <div className="side-area"></div>
-    <div className="main-area">
-      <header className="header"></header>
-      <footer className="footer"></footer>
-    </div>
-  </div>,
+  <App />,
   document.getElementById('root')
 );
