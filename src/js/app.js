bbe57bdac8bfda8ed83042b7d751749359569fb8
tatsuya <yokokura.tatsuya@gmail.com>
2020-03-07 13:20:58 +0900

    4-11-1 コンポーネントファイルの分割

--- src/js/app.js
+++ src/js/app.js
@@ -1,51 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class Footer extends React.Component {
-  render() {
-    return (
-      <footer className="footer">&copy;2020 ReactPractice</footer>
-    );
-  }
-}
-
-class Header extends React.Component {
-  render() {
-    return (
-      <header className="header">受信箱</header>
-    );
-  }
-}
-
-class SideArea extends React.Component {
-  render() {
-    return (
-      <div className="side-area"></div>
-    );
-  }
-}
-
-class MainArea extends React.Component {
-  render() {
-    return (
-      <div className="main-area">
-        <Header />
-        <Footer />
-      </div>
-    );
-  }
-}
-
-class App extends React.Component {
-  render() {
-    return (
-      <div className="wrap">
-        <SideArea />
-        <MainArea />
-      </div>
-    );
-  }
-}
+import App from './components/app';
 
 ReactDOM.render(
   <App />,
