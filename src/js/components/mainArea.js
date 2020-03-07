511ed1e627fede7d7a11fd58040bcae13656131f
tatsuya <yokokura.tatsuya@gmail.com>
2020-03-07 17:40:06 +0900

    4-12-2 ループ処理でDOMを生成

--- src/js/components/mainArea.js
+++ src/js/components/mainArea.js
@@ -5,13 +5,25 @@ import Footer from './footer';
 
 export default class MainArea extends React.Component {
   render() {
+    const todos = [
+      { label: 'Todo1' },
+      { label: 'Todo2' },
+      { label: 'Todo3' },
+    ];
+
+    let todoItemDom = [];
+
+    todos.forEach((todo, index) => {
+      let todoItem = <li className="todo-list-item" key={"item-" + index}>{todo.label}</li>;
+      todoItemDom.push(todoItem);
+    });
+
     return (
       <div className="main-area">
         <Header />
         <main className="list-area">
           <ul className="todo-list">
-            <li className="todo-list-item">Todo1</li>
-            <li className="todo-list-item">Todo2</li>
+            {todoItemDom}
           </ul>
         </main>
         <Footer />
