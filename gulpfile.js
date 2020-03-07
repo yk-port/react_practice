8efa45c2c8c81694e493649d597838ec75eb2a6b
tatsuya <yokokura.tatsuya@gmail.com>
2020-03-07 12:36:47 +0900

    4-10-2 webpackの導入

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,27 @@
 var gulp = require('gulp');
 var watch = require('gulp-watch');
+var plumber = require('gulp-plumber');
+var notify = require('gulp-notify');
 var browserSync = require('browser-sync').create();
+var webpack = require('webpack');
+var webpackStream = require('webpack-stream');
+var webpackConfig = require('./webpack.config');
+
+gulp.task('compile', function(){
+  return gulp.src([
+    './src/js/app.js',
+    ])
+    .pipe(plumber({errorHandler: notify.onError('<%= error.message %>')}))
+    .pipe(webpackStream(webpackConfig), null, function(err, stats){
+      if(stats.compilation.errors.length > 0){
+        notify({
+          title: 'webpack error',
+          message: stats.compilation.errors[0].error
+        });
+      }
+    })
+    .pipe(gulp.dest('js'))
+});
 
 gulp.task('browser-sync', function() {
   browserSync.init({
@@ -11,9 +32,12 @@ gulp.task('browser-sync', function() {
 });
 
 gulp.task('watch', function() {
-  watch(['./**/*.html', './js/**/*.js'], function() {
+  watch(['./src/js/**/*.js'], function() {
+    gulp.start(['compile']);
+  });
+  watch(['./**/*.html', './js/**/*.js', './css/**/.css'], function() {
     browserSync.reload();
   });
 });
 
-gulp.task('default', ['browser-sync', 'watch']);
+gulp.task('default', ['compile', 'browser-sync', 'watch']);
