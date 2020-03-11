e4bed6d31531f81d3723047bed1fd4902a0af94b
tatsuya <yokokura.tatsuya@gmail.com>
2020-03-11 15:57:17 +0900

    8-43 ADD_TODOの処理をreducerに連携

--- src/js/index.js
+++ src/js/index.js
@@ -6,14 +6,6 @@ import configureStore from './store/index';
 
 const store = configureStore();
 
-const onChange = (text) => {
-  const action = {
-    type: "CHANGE_TEXT",
-    text: text
-  }
-  store.dispatch(action);
-}
-
 ReactDOM.render(
   <Provider store={store}>
     <App />
