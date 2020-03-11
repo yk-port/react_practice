5a9dc64ccc032042e4bf53e0667c2199ac01849f
tatsuya <yokokura.tatsuya@gmail.com>
2020-03-11 13:19:07 +0900

    8-41 actionの定義作成

---
+++ src/js/actions/todoActions.js
@@ -0,0 +1,14 @@
+export const todoActionNames = {
+  ADD_TODO: 'ADD_TODO'
+}
+
+export const todoActions = {
+  addTodo: (data) => {
+    return {
+      type: todoActionNames.ADD_TODO,
+      payload: {
+        data: data,
+      }
+    }
+  }
+}
