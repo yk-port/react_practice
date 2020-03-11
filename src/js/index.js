f965dc642e0eb897e4fd80f557febf1e0c90f7c6
tatsuya <yokokura.tatsuya@gmail.com>
2020-03-11 12:56:21 +0900

    8-40 storeとreducerの接続

--- src/js/index.js
+++ src/js/index.js
@@ -1,25 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import App from './components/app';
-
 import configureStore from './store/index';
-
-const reducer = (state = { text: 'text' }, action) => {
-  switch (action.type) {
-    case 'CHANGE_TEXT':
-      return Object.assign({}, state, { text: action.text });
-    default:
-      return state;
-  }
-}
 
 const store = configureStore();
 
 const onChange = (text) => {
   const action = {
-    type: 'CHANGE_TEXT',
-    text: text,
+    type: "CHANGE_TEXT",
+    text: text
   }
   store.dispatch(action);
 }
--- src/js/store/index.js
+++ src/js/store/index.js
@@ -1,5 +1,6 @@
 import { createStore, compose, applyMiddleware } from 'redux';
 import { createLogger } from 'redux-logger';
+import reducer from '../reducers/reducer';
 
 export default function configureStore() {
   const logger = createLogger();
