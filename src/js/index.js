f8caaaa781256505903e54ca9106239901fa8f97
tatsuya <yokokura.tatsuya@gmail.com>
2020-03-13 13:29:57 +0900

    feat(Reducer): storeでreducerの読み込み

--- src/js/index.js
+++ src/js/index.js
@@ -4,16 +4,6 @@ import ReactDOM from 'react-dom';
 import App from './components/app';
 import configureStore from './store/index';
 
-const reducer = (state = { text: 'text' }, action) => {
-  console.log(action);
-  switch (action.type) {
-    case 'CHANGE_TEXT':
-      return Object.assign({}, state, {text: action.text});
-    default:
-      return state;
-  }
-}
-
 const store = configureStore();
 
 const onChange = (text) => {
--- src/js/store/index.js
+++ src/js/store/index.js
@@ -1,6 +1,8 @@
 import { createStore, compose, applyMiddleware } from 'redux';
 import { createLogger } from 'redux-logger';
 
+import reducer from '../reducers/reducer';
+
 export default function configureStore() {
   const logger = createLogger();
 
