fa38b0267fab8244467dddb5734791404a3e707e
tatsuya <yokokura.tatsuya@gmail.com>
2020-03-13 22:34:45 +0900

    refactor(Reducer): ファイルの分割

---
+++ src/js/reducers/groupReducer.js
@@ -0,0 +1,56 @@
+import { groupActionNames } from '../actions/groupActions';
+
+import _ from 'lodash';
+
+const groupInitState = {
+  groupList: [
+    { id: 'inbox', label: '受信箱' },
+    { id: 'group-1', label: 'グループ1' },
+    { id: 'group-2', label: 'グループ2' },
+  ],
+  selectedGroup: 'inbox',
+  groupCount: 2,
+}
+
+function groupReducer(state = groupInitState, action) {
+  let _state = _.cloneDeep(state);
+
+  switch (action.type) {
+    case groupActionNames.ADD_GROUP:
+      _state.groupCount++;
+      let groupItem = {
+        id: action.payload.groupId,
+        label: action.payload.data,
+      }
+      _state.groupList.push(groupItem);
+      return _state;
+
+    case groupActionNames.SELECT_GROUP:
+      _state.selectedGroup = action.payload.id;
+      return _state;
+
+    case groupActionNames.EDIT_GROUP:
+      _state.groupList.forEach(group => {
+        if (group.id == action.payload.id) {
+          group.label = action.payload.groupName;
+        }
+      });
+      return _state;
+
+    case groupActionNames.DELETE_GROUP:
+      _state.groupList.forEach((group, index) => {
+        if (group.id == action.payload.id) {
+          _state.groupList.splice(index, 1);
+        }
+      });
+      if (_state.selectedGroup == action.payload.id) {
+        _state.selectedGroup = 'inbox';
+      }
+      return _state;
+
+    default:
+      return state;
+  }
+}
+
+export default groupReducer;
