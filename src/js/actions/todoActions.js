8fd093e7e0e577141902bd8acc735569f8abb01a
tatsuya <yokokura.tatsuya@gmail.com>
2020-03-13 18:52:00 +0900

    feat(Actions): complete, deleteの追加

--- src/js/actions/todoActions.js
+++ src/js/actions/todoActions.js
@@ -1,5 +1,7 @@
 export const todoActionNames = {
   ADD_TODO: 'ADD_TODO',
+  COMPLETE_TODO: 'COMPLETE_TODO',
+  DELETE_TODO: 'DELETE_TODO',
 }
 
 export const todoActions = {
@@ -8,6 +10,22 @@ export const todoActions = {
       type: todoActionNames.ADD_TODO,
       payload: {
         data: data,
+      }
+    }
+  },
+  completeTodo: (id) => {
+    return {
+      type: todoActionNames.COMPLETE_TODO,
+      payload: {
+        id: id,
+      }
+    }
+  },
+  deleteTodo: (id) => {
+    return {
+      type: todoActionNames.DELETE_TODO,
+      payload: {
+        id: id,
       }
     }
   }
