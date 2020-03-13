37df579da5b83dea9202759ddb41f56b872db899
tatsuya <yokokura.tatsuya@gmail.com>
2020-03-13 09:08:16 +0900

    build: redux-loggerの導入

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
+import { createStore, compose, applyMiddleware } from 'redux';
+import { createLogger } from 'redux-logger';
 
 // import App from './components/app';
 import MainArea from './components/mainArea';
@@ -15,9 +16,14 @@ const reducer = (state = { text: 'text' }, action) => {
   }
 }
 
+const logger = createLogger();
+
 const store = createStore(
   reducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  compose(
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+    applyMiddleware(logger)
+  )
 );
 
 const onChange = (text) => {
